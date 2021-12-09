20e35ec9aef699d59ecb2fd587d3f6deded014e9
Veronchi <zhigunova1995@gmail.com>
2021-12-09 17:56:47 +0300

    add questions content change

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React, { useState } from 'react';
 import data from './data';
 import SingleQuestion from './Question';
+
 function App() {
+  const [questions, setQuestions] = useState(data);
+
   return (
     <main>
       <div className="container">
         <h3>questions and answer about login</h3>
-        <SingleQuestion />
+        <section className="info">
+          {questions.map((question) => {
+            return (
+              <SingleQuestion key={question.id} {...question} />
+            );
+          })}
+        </section>
       </div>
     </main>
   );
--- src/Question.js
+++ src/Question.js
@@ -1,7 +1,22 @@
 import React, { useState } from 'react';
-import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
-const Question = () => {
-  return <h2>question component</h2>;
+import {ReactComponent as Plus} from './plus.svg'
+import {ReactComponent as Minus} from './minus.svg'
+
+const Question = ({ title, info }) => {
+
+  const [isFullContent, setIsFullContent] = useState(false);
+
+  return (
+    <article className="question">
+      <header>
+        <h4>{title}</h4>
+        <button onClick={() => setIsFullContent(!isFullContent)} className="btn">
+          {isFullContent ? <Minus /> : <Plus />}
+        </button>
+      </header>
+      {isFullContent? <p>{info}</p> : null}
+    </article>
+  );
 };
 
 export default Question;
