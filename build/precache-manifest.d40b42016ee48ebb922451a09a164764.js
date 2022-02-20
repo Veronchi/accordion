6af02916a631a5f215f9b90d1502af63470dfc5e
Veronchi <zhigunova1995@gmail.com>
2022-02-20 17:34:02 +0300

    add build folder

---
+++ build/precache-manifest.d40b42016ee48ebb922451a09a164764.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c54d1bc72e3cc83754d680d66dcbce1a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "82b8c79628f1151d08a8",
+    "url": "./static/css/main.4b589e59.chunk.css"
+  },
+  {
+    "revision": "d1ccc1e6d556dbe2d9b7",
+    "url": "./static/js/2.d8a71053.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.d8a71053.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "82b8c79628f1151d08a8",
+    "url": "./static/js/main.8b4f280c.chunk.js"
+  },
+  {
+    "revision": "634688380851e0148535",
+    "url": "./static/js/runtime-main.9a51be4d.js"
+  },
+  {
+    "revision": "64c00dede07719ecc2b565e5003a5226",
+    "url": "./static/media/minus.64c00ded.svg"
+  },
+  {
+    "revision": "909871113c4112c058116d76915ce877",
+    "url": "./static/media/plus.90987111.svg"
+  }
+]);
